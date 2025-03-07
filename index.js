6fa610454b7afb7d981fae8818f8e431b6a95f49
laboud <yespro2019@gmail.com>
2025-03-07 21:26:48 +0100

    7

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
 let slideIndex = 0;
-showSlides();
 
 function showSlides() {
-    let i;
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
+
+    if (slides.length === 0 || dots.length === 0) return; 
+
+ 
+    for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
     }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+
+
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active");
     }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
+
+    slideIndex = (slideIndex + 1 > slides.length) ? 1 : slideIndex + 1;
+    slides[slideIndex - 1].style.display = "block";  
+    dots[slideIndex - 1].classList.add("active");
+
     setTimeout(showSlides, 2000); 
 }
+
+document.addEventListener("DOMContentLoaded", showSlides);
+          
+let btnOM = document.querySelector("button.btn-open-menu");
+let btnCM = document.querySelector("button.btn-close-menu");
+let boxM = document.querySelector("div.box-menu");
+
+btnOM.addEventListener("click", () => {
+  boxM.classList.toggle("active");
+});
+
+btnCM.addEventListener("click", () => {
+  boxM.classList.remove("active");
+});
+
+document.addEventListener("click", event => {
+  if (!btnOM.contains(event.target) && event.target !== btnOM) {
+    boxM.classList.remove("active");
+  };
+});
+          
+      
